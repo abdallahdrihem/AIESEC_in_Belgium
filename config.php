55995fb63d8448024c2d42e8d79bb524d422e814
Abdallah Drihem <samidrihem@gmail.com>
2016-12-15 02:59:57 -0800

    Create config.php

---
+++ config.php
@@ -0,0 +1,17 @@
+<?php
+
+return array(
+    //Labels of registration form fields
+    'name' => 'Your Name',
+    'surname' => 'Your surname',
+    'e-mail' => 'Your e-mail',
+    'password' => 'Set password',
+	'phone' => 'phone',
+    'lead-name' => 'My name',
+    'lc' => 'Local Committee',
+    'thank-you-message' => 'Thank you very much for registration. Soon you will recive E-mail with description of next steps.',
+    'country_name' => 'BELGIUM',
+    'mc_id' => '1624'
+);
+
+?>
